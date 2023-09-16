bdfb6ee43135a10e66f0d38cc952c1417adcf4b5
zowu <luke.wu@nokia-sbell.com>
2023-09-16 11:14:22 +0800

    test

--- index.js
+++ index.js
@@ -4,12 +4,15 @@ const core = require('@actions/core');
 
 function verifyConditions() {
   // core.setOutput("new-release-published", "false");
-  process.env.newReleasePublished = "false";
+  // process.env.newReleasePublished = "false";
+  core.exportVariable("newReleasePublished", "false");
 }
 
 function generateNotes(_pluginConfig, { nextRelease }) {
-  process.env.newReleasePublished = "true";
-  process.env.newRelease = nextRelease.version;
+  // process.env.newReleasePublished = "true";
+  // process.env.newRelease = nextRelease.version;
+  core.exportVariable("newReleasePublished", "true");
+  core.exportVariable("newRelease", nextRelease.version);
   // core.setOutput("new-release-published", "true");
   // core.setOutput("new-release-version", nextRelease.version);
 }
